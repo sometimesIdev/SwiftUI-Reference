ca0d3ba7e020967dd41da88e9e59594fe116d06e
Pat Butler <pcbutler@telus.net>
2022-08-09 18:03:03 -0700

    Initial Commit

---
+++ SwiftUI Reference/SwiftUI_ReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ReferenceApp.swift
+//  SwiftUI Reference
+//
+//  Created by Pat Butler on 2022-08-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
